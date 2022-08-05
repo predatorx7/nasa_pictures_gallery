f936d1ed94bd6f7c0093c490f2154ce2d0cdf4de
predatorx7 <smushaheed@outlook.com>
2022-08-05 14:09:32 +0530

    Add updated typography in themes

--- lib/commons/theme.dart
+++ lib/commons/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppStyles {
   AppStyles._();
@@ -24,5 +25,23 @@ class AppStyles {
     primaryColor: colorScheme.primary,
     useMaterial3: true,
     splashFactory: kIsWeb ? null : InkSparkle.splashFactory,
+    typography: typography,
+  );
+
+  static TextTheme textTheme = GoogleFonts.openSansTextTheme(
+    Typography.englishLike2021,
+  );
+
+  static Typography typography = Typography.material2021(
+    black: textTheme.apply(
+      bodyColor: Colors.black,
+      displayColor: Colors.black,
+      decorationColor: Colors.black,
+    ),
+    white: textTheme.apply(
+      bodyColor: Colors.white,
+      displayColor: Colors.white,
+      decorationColor: Colors.white,
+    ),
   );
 }
